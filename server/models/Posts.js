9724446365f2fe574d8512d33c0a0f233b49f898
Seba Fox <seba.n.fox@gmail.com>
2024-09-23 15:34:55 -0400

    API Routes set up, User Registration

--- server/models/Posts.js
+++ server/models/Posts.js
@@ -17,5 +17,11 @@ module.exports = (sequelize, DataTypes) => {
         },
     });
 
+    Posts.associate = (models) => {
+        Posts.hasMany(models.Comments, {
+            onDelete: "cascade"
+        });
+    };
+
     return Posts
-}
+};
--- server/routes/Posts.js
+++ server/routes/Posts.js
@@ -3,17 +3,20 @@ const router = express.Router();
 const { Posts } = require("../models");
 
 router.get("/", async (req, res) => {
-  const listOfPosts = await Posts.findAll(); //Allows us to view a list of every post ever made
+  const listOfPosts = await Posts.findAll();
   res.json(listOfPosts);
 });
 
+router.get("/byId/:id", async (req, res) => {
+  const id = req.params.id;
+  const post = await Posts.findByPk(id);
+  res.json(post);
+});
 
 router.post("/", async (req, res) => {
-  const post = req.body; //Allows us to create post from the server side 
+  const post = req.body;
   await Posts.create(post);
   res.json(post);
-}); 
-
-
+});
 
 module.exports = router;
