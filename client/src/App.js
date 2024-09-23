6d09e00f72361bd6ba474c41635c893b08caa6e4
Seba Fox <seba.n.fox@gmail.com>
2024-09-23 11:13:58 -0400

    Create post and Home Page are set up

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Link to="/createpost"> Create A Post</Link>
+        <Link to="/"> Home Page</Link>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/createpost" element={<CreatePost/>} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
